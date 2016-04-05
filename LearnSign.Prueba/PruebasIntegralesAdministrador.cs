86ecb2ec7e4d0c2fcd942042b7427ee86e83f8c3
William <williamupt@outlook.com>
2016-04-04 23:54:33 -0500

    SE MODIFICA EL MÉTODO DE PRUEBA DEL ADMINISTRADOR

--- LearnSign.Prueba/PruebasIntegralesAdministrador.cs
+++ LearnSign.Prueba/PruebasIntegralesAdministrador.cs
@@ -7,74 +7,19 @@ using Learnsign.Infraestructura;
 
 namespace LearnSign.Prueba
 {
-    /// <summary>
-    /// Descripción resumida de PruebasIntegralesAdministrador
-    /// </summary>
     [TestClass]
     public class PruebasIntegralesAdministrador
     {
-        public PruebasIntegralesAdministrador()
-        {
-            //
-            // TODO: Agregar aquí la lógica del constructor
-            //
-        }
-
-        private TestContext testContextInstance;
-
-        /// <summary>
-        ///Obtiene o establece el contexto de las pruebas que proporciona
-        ///información y funcionalidad para la serie de pruebas actual.
-        ///</summary>
-        public TestContext TestContext
-        {
-            get
-            {
-                return testContextInstance;
-            }
-            set
-            {
-                testContextInstance = value;
-            }
-        }
-
-        #region Atributos de prueba adicionales
-        //
-        // Puede usar los siguientes atributos adicionales conforme escribe las pruebas:
-        //
-        // Use ClassInitialize para ejecutar el código antes de ejecutar la primera prueba en la clase
-        // [ClassInitialize()]
-        // public static void MyClassInitialize(TestContext testContext) { }
-        //
-        // Use ClassCleanup para ejecutar el código una vez ejecutadas todas las pruebas en una clase
-        // [ClassCleanup()]
-        // public static void MyClassCleanup() { }
-        //
-        // Usar TestInitialize para ejecutar el código antes de ejecutar cada prueba 
-        // [TestInitialize()]
-        // public void MyTestInitialize() { }
-        //
-        // Use TestCleanup para ejecutar el código una vez ejecutadas todas las pruebas
-        // [TestCleanup()]
-        // public void MyTestCleanup() { }
-        //
-        #endregion
-
         [TestMethod]
         public void AgregarAdministrador()
         {
             var lo_administrador = Adminsitrador.Agregar(1, 70653666, "Edwin", "Condori Vilcapuma", "edwin@gmail.com", "Zela 666", 052311413, 974858666, 1, "123456");
             using (var lo_contexto = new LearnSignContexto())
             {
-
                 lo_contexto.Administradores.Add(lo_administrador);
                 lo_contexto.SaveChanges();
-
             }
-            Assert.IsTrue(lo_administrador.IdAdministrador == 1);
-            //
-            // TODO: Agregar aquí la lógica de las pruebas
-            //
+            Assert.IsTrue(lo_administrador != null);
         }
     }
 }
