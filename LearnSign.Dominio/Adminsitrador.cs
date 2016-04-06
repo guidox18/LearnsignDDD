42ca9576ea29208ff5c2ea875f16ff70915faf0a
godartg <sl.vilcapuma.edwin@gmail.com>
2016-04-06 18:30:11 -0500

    Esto tengo que hacer en todos

--- LearnSign.Dominio/Adminsitrador.cs
+++ LearnSign.Dominio/Adminsitrador.cs
@@ -41,5 +41,18 @@ namespace LearnSign.Dominio
                 Pass = as_pass_admin
             };
         }
+
+        /// <summary>
+        /// Este método actualiza datos del alumno
+        /// </summary>
+        /// <param name="as_nom_alumno"></param>
+        /// <param name="as_apell_alumno"></param>
+        /// <param name="as_email_alum"></param>
+        /*public void Actualizar(string as_nom_alumno, string as_apell_alumno, string as_email_alum)
+        {
+            Nombre = as_nom_alumno;
+            Apellidos = as_apell_alumno;
+            Email = as_email_alum;
+        }*/
     }
 }
